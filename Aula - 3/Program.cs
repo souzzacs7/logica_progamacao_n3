9db904b400ac9e81ee169aacbc7f64a32f012548
CARLOS EMANUEL DE OLIVEIRA SOUZA <carlos.eosouza7@senacsp.edu.br>
2025-11-03 20:42:29 -0300

    primeiro commit com codigo

---
+++ Aula - 3/Program.cs
@@ -0,0 +1,68 @@
+﻿// Aula 3 - OPERADORES LÓGICOS
+
+/*int x = 1;
+int y = 1;
+int z = 2;
+
+double resultado = ((x + y) * z) / 3.0;
+
+resultado += 100;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Olá, Mundo");
+Console.WriteLine(resultado < 100);
+
+Console.WriteLine("\n-----\n");
+
+string nome = "Rafael";
+string sobrenome = "Sousa";
+Console.WriteLine(nome + sobrenome);
+
+char letraA = '0';
+char letraB = '1';
+Console.WriteLine(letraA + 0);
+Console.WriteLine(letraB + 0);
+
+Console.WriteLine("\n-----\n");
+
+Console.WriteLine("Escreva seu nome.");
+string nomeUsuario = Console.ReadLine();
+Console.WriteLine("O seu nome é: " + nomeUsuario);
+
+Console.WriteLine("Em qual ano você nasceu?");
+string anoNascimentoUsuarioString = Console.ReadLine();
+
+int IdadeUsuario = 2025 - int.Parse(idadeUsuario);
+
+Console.WriteLine("A sua idade é: " + idadeUsuario);*/
+
+//int IdadeUsuario = int.Parse(idadeUsuarioString);
+
+//Atividade - Resolução e revisão do professor.
+
+//Peça para o usuário inserir dois números inteiros e mostre-os no Console.
+
+int n1;
+int n2;
+
+Console.WriteLine("Informe um número inteiro");
+n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe outro número inteiro");
+n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Os números informados são:" + n1 + " e " + n2 + ".");
+
+//Peça para o usuário inserir dois números inteiros, mostre a soma dos números informados no Console.
+
+
+
+//Peça para o usuário inserir dois números inteiros, mostre a subtração dos números informados no Console.
+
+//Peça para o usuário inserir dois números inteiros, mostre a multiplicação dos números informados no Console.
+
+//Peça para o usuário inserir dois números inteiros, mostre a divisão dos números informados no Console (Cuidado com o tipo na hora de mostrar o resultado).
+
+
+
+
---
+++ Aula2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Aula 2 - Lógica de Programação
+ctrl k + c -> LINHA DE COMENTÁRIO
+ctrl k + u -> RETIRA LINHA DE COMENTÁRIO
+ctrl + shift + b -> COMPILAÇÃO */
+
+
+
+// DECLARAÇÃO DE UMA VARIÁVEL
+string nome;
+
+// ATRIBUIÇÃO DE VALOR A UMA VARIÁVEL
+nome = "Dark Souls";
+
+Console.WriteLine(nome);
+
+nome = "Soul Of Cinder";
+
+Console.WriteLine(nome);
+
+// DECLARAÇÃO + ATRIBUIÇÃO EM UM COMANDO SÓ PARA VARIÁVEIS CONSTANTES
+const string constante = "minha constante";
+
+Console.WriteLine(constante);
+
+// tipo nome = valor;
+
+int idade = 18;
+
+long inteiroGrande = 0;
+
+float alturaFloat = 1.7; // - ponto flutuante
+
+double altura = 1.7;
+
+char letra = 'A';
+
+bool boleano = true;
+
