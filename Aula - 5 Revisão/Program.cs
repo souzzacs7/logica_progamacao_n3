12949f13286e762565bc14a2f93997c19729fea5
CARLOS EMANUEL DE OLIVEIRA SOUZA <carlos.eosouza7@senacsp.edu.br>
2025-11-10 19:25:47 -0300

    aula5

---
+++ Aula - 5 Revisão/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//Peça para o usuário inserir dois números inteiros e mostre-os no Console.
+
+int n1;
+int n2;
+int resultado;
+// Escrevi uma mensagem no Console.
+//Console.WriteLine("Insira um número inteiro: ");
+// Li o conteúdo de um linha, quando o usuário apertou a tecla ENTER
+// O Console só aceita texto (string)
+// Logo é preciso converter a string para int
+// Parse é usado para converter string para int (não somente para isso)
+//n1 = int.Parse(Console.ReadLine());
+
+// Escrevi outra mensagem no Console.
+//Console.WriteLine("Insira mais um número inteiro: ");
+// Novamente li o conteudo da linha, quando o usuário apertou a tecla ENTER
+//n2 = int.Parse(Console.ReadLine());
+
+// Mostra os dois números no console.
+//Console.WriteLine("Seus números são: " + n1 + " e " + n2);
+//---------------------------------------------------------------------
+
+
+// Exercício 2 - Soma dos números que o usuário solicitou.
+//Console.WriteLine("Escreva um número inteiro");
+//n1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Escreva outro número inteiro");
+//n2 = int.Parse(Console.ReadLine());
+
+//// Agora a parte da soma.
+
+//resultado = n1 + n2;
+//// Denominei a operação "soma" como "resultado" para melhorar o entendimento. 
+//Console.WriteLine("A soma dos seus dois números é:" + resultado);
+
+//-----------------------------------------------------------------------
+
+//Exercicio 3 - Subtração
+
+//Console.WriteLine("Escreva um número inteiro");
+//n1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Escreva outro número inteiro");
+//n2 = int.Parse(Console.ReadLine());
+
+//resultado = n1 - n2;
+
+//Console.WriteLine("O resultado de sua subtração é: " + resultado);
+
+//-----------------------------------------------------------------------
+
+//Exercicio 4 - Multiplicação.
+
+//Console.WriteLine("Escreva um número inteiro");
+//n1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Escreva outro número inteiro");
+//n2 = int.Parse(Console.ReadLine());
+
+//resultado = n1 * n2;
+//Console.WriteLine("O resultado de sua multiplicação é: " + resultado);
+
+//-----------------------------------------------------------------------
+
+//Exercicio 5 - Divisão.
+
+//double n3;
+//double n4;
+//double resultado2;
+
+
+//Console.WriteLine("Escreve um número inteiro");
+//n3 = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Escreva outro número inteiro");
+//n4 = double.Parse(Console.ReadLine());
+
+//resultado2 = n3 / n4;
+
+//Console.WriteLine("O Resultado é igual a: " + resultado2 );
+
+
+
+
+
