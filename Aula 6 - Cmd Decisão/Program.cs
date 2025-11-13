051fed71b5b5381864cfe602173f128c03dbfc61
CARLOS EMANUEL DE OLIVEIRA SOUZA <carlos.eosouza7@senacsp.edu.br>
2025-11-12 21:47:23 -0300

    exercicios

---
+++ Aula 6 - Cmd Decisão/Program.cs
@@ -0,0 +1,38 @@
+﻿// Definir as constantes
+const int DEZOITO = 18;
+const int ZERO = 0;
+
+// Escrevendo no console uma string pedindo a idade do usuario
+Console.WriteLine("Qual a sua idade?");
+
+// Ler a resposta do usuario
+string resposta = Console.ReadLine();
+
+// Tenta transformar a string resposta em um int idade,
+// atribui o resultado a uma variavel bool
+bool ehValido = int.TryParse(resposta, out int idade);
+
+// Verifica se a tentativa de parse funcionou
+if (ehValido)
+{
+    // Verifica se idade é maior ou igual a 18
+    if (idade >= DEZOITO)
+    {
+        Console.WriteLine("Usuário é maior de idade.");
+    }
+    // Verifica se a idade é válida
+    else if (idade < ZERO)
+    {
+        Console.WriteLine("Idade inválida");
+    }
+    // Se for valida e menor que 18
+    else
+    {
+        Console.WriteLine("Usuário é menor de idade");
+    }
+}
+// Caso não tenha funcionado
+else
+{
+    Console.WriteLine("Valor informado não corresponde a um número inteiro");
+}
---
+++ Aula 7 - Exercício/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Ler um número inteiro e exibir o dobro do mesmo.
+
+//const int DOBRO = 2;
+
+//Console.WriteLine("Escolha um número inteiro");
+//int.Parse(Console.ReadLine());
+
+//Console.WriteLine("O dobro desse número é");
+//int.Parse(Console.ReadLine());
+
+//------------------------------------------------------------------------------------------'
+
+//Console.WriteLine("Escreva um número inteiro e após isso você irá visualizar o dobro dele: ");
+//String inteiro = Console.ReadLine();
+//int num = int.Parse(inteiro);
+//int dobro = num * 2;
+//Console.WriteLine("o Dobro desse número " + inteiro + " é " + dobro);
+
+//------------------------------------------------------------------------------------------'
+
+//Exercicio 2 - Parte 1 
+
+//Console.WriteLine("Digite o lado do quadrado ");
+
+//String tamn = Console.ReadLine();
+
+//int tam = int.Parse(tamn);
+
+//int per = tam * 4;
+
+//int area = tam * tam;
+
+//Console.WriteLine("o Perímetro do quadrado é " + per + " e a area " + area);
+
+//------------------------------------------------------------------------------------------'
+
+//Exercicio 3 - Ler um número inteiro e exibir se ele é impar ou par.
+//
+Console.WriteLine("Escreva um número qualquer, inteiro ou não");
+double num = double.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+
+    Console.WriteLine(num + " Este número é Par ");
+
+}
+else
+{
+    Console.WriteLine(num + " Este número é Impar ");
+}
+
